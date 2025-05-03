9373694dfae70c7b7c8d5c2d3a41237a899c5696
jcfrongoso <13251277+smurfedGitHubIO@users.noreply.github.com>
2025-05-03 14:02:20 +0800

    feat: minor fixes

--- background.js
+++ background.js
@@ -1,1 +1,33 @@
-console.log("Site Blocker Extension loaded");
+chrome.runtime.onInstalled.addListener(() => {
+  console.log("Site Blocker installed");
+  
+  const blockedSites = [
+    "instagram.com",
+    "x.com",
+    "twitter.com",
+    "reddit.com",
+    "youtube.com",
+    "www.youtube.com"
+  ];
+
+  const rules = blockedSites.map((site, index) => ({
+    id: 1000 + index,
+    priority: 1,
+    action: { type: "block" },
+    condition: {
+      urlFilter: site,
+      resourceTypes: ["main_frame", "sub_frame"]
+    }
+  }));
+  
+  chrome.declarativeNetRequest.updateDynamicRules({
+    removeRuleIds: rules.map(rule => rule.id),
+    addRules: rules
+  });
+});
+
+chrome.declarativeNetRequest.onRuleMatchedDebug?.addListener(
+  (info) => {
+    console.log("Blocked request:", info);
+  }
+);
